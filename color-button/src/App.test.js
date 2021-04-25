b1b136fdfcf6c7e4a4f26e5eceb8acaeb78929f1
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-04-25 11:56:16 -0300

    color-button: Test and code checkbox functionality

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -2,9 +2,16 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 let colorButton;
+let checkBox;
 beforeEach(() => {
-    render(<App />);
-    colorButton = screen.getByRole("button", { name: "Change to blue" });
+  render(<App />);
+  colorButton = screen.getByRole("button", { name: "Change to blue" });
+  checkBox = screen.getByRole("checkbox");
+});
+
+test("initial conditions", () => {
+  expect(colorButton).toBeEnabled();
+  expect(checkBox).not.toBeChecked();
 });
 
 test("button has correct initial color", () => {
@@ -21,4 +28,19 @@ test("button has correct color when clicked once", () => {
   fireEvent.click(colorButton);
 
   expect(colorButton.textContent).toBe("Change to red");
+});
+
+test("disable button on checkbox click", () => {
+  fireEvent.click(checkBox);
+
+  expect(checkBox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+});
+
+test("re-enable button on second checkbox click", () => {
+  fireEvent.click(checkBox);
+  fireEvent.click(checkBox);
+
+  expect(checkBox).not.toBeChecked();
+  expect(colorButton).toBeEnabled();
 });
