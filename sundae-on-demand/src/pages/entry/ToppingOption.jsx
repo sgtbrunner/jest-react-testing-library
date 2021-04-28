0d81240f4432429dfca0070d5ff7b2d201601747
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-04-28 09:36:52 -0300

    Add toppings test and code implementation

--- sundae-on-demand/src/pages/entry/ToppingOption.jsx
+++ sundae-on-demand/src/pages/entry/ToppingOption.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
 
-const ScoopOption = ({ name, imagePath }) => (
+const ToppingOption = ({ name, imagePath }) => (
   <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
     <img
       style={{ width: "75%" }}
       src={`http://localhost:3030/${imagePath}`}
-      alt={`${name} scoop`}
+      alt={`${name} topping`}
     />
   </Col>
 );
 
-export default ScoopOption;
+export default ToppingOption;
--- sundae-on-demand/src/pages/entry/__test__/Options.test.jsx
+++ sundae-on-demand/src/pages/entry/__test__/Options.test.jsx
@@ -2,17 +2,40 @@ import { render, screen } from "@testing-library/react";
 
 import Options from "../Options";
 
-let scoopImages;
-beforeEach(async () => {
-  render(<Options optionType="scoops" />);
-  scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+describe("scoop images", () => {
+  let scoopImages;
+  beforeEach(async () => {
+    render(<Options optionType="scoops" />);
+    scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  });
+
+  test("displays two(2) scoop images", () => {
+    expect(scoopImages).toHaveLength(2);
+  });
+
+  test("displays image for each scoop option from server", () => {
+    const altText = scoopImages.map((element) => element.alt);
+    expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+  });
 });
 
-test("displays two(2) scoop images", () => {
-  expect(scoopImages).toHaveLength(2);
-});
+describe("topping option", () => {
+  let toppingImages;
+  beforeEach(async () => {
+    render(<Options optionType="toppings" />);
+    toppingImages = await screen.findAllByRole("img", { name: /topping$/i });
+  });
 
-test("displays image for each scoop option from server", () => {
-  const altText = scoopImages.map((element) => element.alt);
-  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+  test("displayes three(3) topping images", () => {
+    expect(toppingImages).toHaveLength(3);
+  });
+
+  test("displays image for each scoop option from server", () => {
+    const altText = toppingImages.map((element) => element.alt);
+    expect(altText).toEqual([
+      "Cherries topping",
+      "M&Ms topping",
+      "Hot fudge topping",
+    ]);
+  });
 });
